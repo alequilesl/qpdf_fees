dded2b3bb6a54e6ceebafded5f425d732e6df898
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2026-06-02 15:06:46 -0400

    se quedo el adjunto jj

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-// config.js
-const APP_CONFIG = {
-    // URL del sitio (usar en botones de "Volver al Inicio")
-    siteUrl: 'https://qpdf.alequiles.com',
-    
-    // URL de descarga de la app
-    downloadUrl: 'https://downloads.alequiles.com/qpdf/latest/qpdf-setup.exe',
-    
-    // Versión de la app
-    appVersion: '2.1.0'
+const QPDF_CONFIG = {
+    siteUrl: "https://qpdf.app",
+    downloadUrl: "https://qpdf.app/download",
+    appVersion: "2.1.0"
 };
