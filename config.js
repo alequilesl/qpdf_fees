0d5df67145cb1c0556f07a5c60d66b43164d747c
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2026-06-02 15:05:40 -0400

    config centralizada

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const APP_CONFIG = {
+    // URL del sitio (usar en botones de "Volver al Inicio")
+    siteUrl: 'https://qpdf.alequiles.com',
+    
+    // URL de descarga de la app
+    downloadUrl: 'https://downloads.alequiles.com/qpdf/latest/qpdf-setup.exe',
+    
+    // Versión de la app
+    appVersion: '2.1.0'
+};
