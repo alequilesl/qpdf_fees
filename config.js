f830eb7bfa4fd206d144d85907ac540b958334f3
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2026-06-04 18:09:46 -0400

    update url apk

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const QPDF_CONFIG = {
     siteUrl: "https://alequilesl.github.io/qpdf_fees/",
-    downloadUrl: "https://qpdf.app/download",
+    downloadUrl: "https://drive.google.com/file/d/11e4Ep5Cf_-1Z_Zsn8xQZhyrVgqSFKiZs/view?usp=sharing",
     appVersion: "2.0.0"
 };
