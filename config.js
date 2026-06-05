1c75191cb90e0c749b35198416a15c353781410b
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2026-06-05 16:19:58 -0400

    update download link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const QPDF_CONFIG = {
     siteUrl: "https://alequilesl.github.io/qpdf_fees/",
-    downloadUrl: "https://drive.google.com/file/d/13gDYF9nyjtGj16xCFQCTd0a_J7vjRYDf/view?usp=sharing",
+    downloadUrl: "https://drive.google.com/file/d/11j5VRtmwhhb3t_YBhgGReceainentqBp/view?usp=sharing",
     appVersion: "2.0.0"
 };
