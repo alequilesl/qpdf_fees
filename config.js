d7cce4f8fb171a7d5bbe2a98e4b1530e982de38e
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2026-06-25 13:07:56 -0400

    2.2.1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const QPDF_CONFIG = {
     siteUrl: "https://alequilesl.github.io/qpdf_fees/",
     downloadUrl: "https://www.apklis.cu/application/com.aql.qpdf",
-    appVersion: "2.1.0"
+    appVersion: "2.2.1"
 };
